56f6763f1834bb07b8f3a5ecfc94345d0aaf3772
Jesus de la Luz V <jesus@pop-os.localdomain>
2022-11-17 22:09:37 -0700

    Deploy for test react app 2022-11-17

--- app.js
+++ app.js
@@ -12,29 +12,30 @@ app.use( express.static( __dirname + '/public' ) );
 app.set( 'view engine', 'hbs' );
 hbs.registerPartials( __dirname + '/views/partials' );
 
-app.get( '/', ( req, res ) => {
-  res.render('home', {
-    user_name: 'Test User',
-    page_title: 'Node Course',
-  });
-});
+// app.get( '/', ( req, res ) => {
+//   res.render('home', {
+//     user_name: 'Test User',
+//     page_title: 'Node Course',
+//   });
+// });
 
-app.get( '/generic', ( req, res ) => {
-  res.render('generic', {
-    user_name: 'Test User',
-    page_title: 'Node Course',
-  });
-});
+// app.get( '/generic', ( req, res ) => {
+//   res.render('generic', {
+//     user_name: 'Test User',
+//     page_title: 'Node Course',
+//   });
+// });
 
-app.get( '/elements', ( req, res ) => {
-  res.render('elements', {
-    user_name: 'Test User',
-    page_title: 'Node Course',
-  });
-});
+// app.get( '/elements', ( req, res ) => {
+//   res.render('elements', {
+//     user_name: 'Test User',
+//     page_title: 'Node Course',
+//   });
+// });
 
 app.get( '*', ( req, res ) => {
-  res.sendFile( __dirname + '/public/404.html' );
+  // res.sendFile( __dirname + '/public/404.html' );
+  res.sendFile( __dirname + '/public/index.html' );
 });
 
 app.listen( port, () => {
